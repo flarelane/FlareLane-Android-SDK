ccf2fe5d119605618f059c3f0c93ed0c552d015c
MinHyeok Kim <minhyeok4dev@gmail.com>
2021-11-16 18:30:49 +0900

    Activate callback is registered on onActivityPaused

--- FlareLane/src/main/java/com/flarelane/ActivityLifecycleManager.java
+++ FlareLane/src/main/java/com/flarelane/ActivityLifecycleManager.java
@@ -13,22 +13,6 @@ class ActivityLifecycleManager {
     protected Application.ActivityLifecycleCallbacks mActivityLifecycleCallbacks = new Application.ActivityLifecycleCallbacks() {
         @Override
         public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
-            if (isActivated == false) {
-                Context context = activity.getApplicationContext();
-                String projectId = com.flarelane.BaseSharedPreferences.getProjectId(context);
-                String deviceId = com.flarelane.BaseSharedPreferences.getDeviceId(context);
-                String pushToken = com.flarelane.BaseSharedPreferences.getPushToken(context);
-
-                if (projectId != null && deviceId != null && pushToken != null) {
-                    try {
-                        com.flarelane.DeviceService.activate(activity.getApplicationContext(), projectId, deviceId, pushToken);
-                    } catch (Exception e) {
-                        BaseErrorHandler.handle(e);
-                    }
-                    isActivated = true;
-                    com.flarelane.Logger.verbose("App is activated");
-                }
-            }
         }
 
         @Override
@@ -41,6 +25,24 @@ class ActivityLifecycleManager {
 
         @Override
         public void onActivityPaused(@NonNull Activity activity) {
+//            TODO: ReactNative 콜백 등록 타이밍 이슈로 Paused 시 activate 체크
+            if (isActivated == false) {
+                Context context = activity.getApplicationContext();
+                String projectId = com.flarelane.BaseSharedPreferences.getProjectId(context);
+                String deviceId = com.flarelane.BaseSharedPreferences.getDeviceId(context);
+                String pushToken = com.flarelane.BaseSharedPreferences.getPushToken(context);
+
+                if (projectId != null && deviceId != null && pushToken != null) {
+                    com.flarelane.Logger.verbose("App is activated");
+                    isActivated = true;
+
+                    try {
+                        com.flarelane.DeviceService.activate(activity.getApplicationContext(), projectId, deviceId, pushToken);
+                    } catch (Exception e) {
+                        BaseErrorHandler.handle(e);
+                    }
+                }
+            }
         }
 
         @Override
