361f50f0cc57c1a5cc7f51e04e6217afbe206aa6
MinHyeok Kim <minhyeok4dev@gmail.com>
2022-04-01 20:53:29 +0900

    Move firebase variables to strings.xml

--- FlareLane/src/main/java/com/flarelane/FirebaseManager.java
+++ FlareLane/src/main/java/com/flarelane/FirebaseManager.java
@@ -7,20 +7,16 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 class FirebaseManager {
-    private static final String FLARELANE_FIREBASE_APP_ID = "1:415602473946:android:98a2ad40916ab031e59ee3";
-    private static final String FLARELANE_FIREBASE_PROJECT_ID = "flarelane-public";
-    private static final String FLARELANE_FIREBASE_API_KEY = "AIzaSyC44DyZywKLyQH3AVXZrLbGu7VDJosMKQ0";
     private static final String FLARELANE_FIREBASE_APP_NAME = "FirebaseOfFlareLane";
     private static FirebaseApp firebaseApp;
-
 
     public static FirebaseMessaging getFirebaseMessaging(Context context, String senderId) {
         if (firebaseApp == null) {
             FirebaseManager.firebaseApp = FirebaseApp.initializeApp(context,
                     new FirebaseOptions.Builder()
-                            .setApplicationId(FLARELANE_FIREBASE_APP_ID)
-                            .setProjectId(FLARELANE_FIREBASE_PROJECT_ID)
-                            .setApiKey(FLARELANE_FIREBASE_API_KEY)
+                            .setApplicationId(context.getString(R.string.flarelane_firebase_app_id))
+                            .setProjectId(context.getString(R.string.flarelane_firebase_project_id))
+                            .setApiKey(context.getString(R.string.flarelane_firebase_api_key))
                             .setGcmSenderId(senderId)
                             .build(),
                     FLARELANE_FIREBASE_APP_NAME
