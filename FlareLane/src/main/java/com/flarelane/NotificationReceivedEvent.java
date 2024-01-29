9c4918d52d04bd4e0296d3a03d322d5e7f56b5f4
JP <jpyo0613@gmail.com>
2024-01-29 14:07:06 +0900

    notification bigLargeIcon 자리에 타입 캐스팅 없었던 부분 추가

--- FlareLane/src/main/java/com/flarelane/NotificationReceivedEvent.java
+++ FlareLane/src/main/java/com/flarelane/NotificationReceivedEvent.java
@@ -91,16 +91,16 @@ public class NotificationReceivedEvent {
                         if (image != null) {
                             builder = builder
                                     .setLargeIcon(image)
-                                    .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(image).setSummaryText(flarelaneNotification.body));
+                                    .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(image).bigLargeIcon((Bitmap) null).setSummaryText(flarelaneNotification.body));
                         } else {
                             builder = builder.setStyle(new NotificationCompat.BigTextStyle().bigText(flarelaneNotification.body));
                         }
 
                         android.app.Notification notification = builder.build();
 
-                        notification.defaults|= android.app.Notification.DEFAULT_SOUND;
-                        notification.defaults|= android.app.Notification.DEFAULT_LIGHTS;
-                        notification.defaults|= android.app.Notification.DEFAULT_VIBRATE;
+                        notification.defaults |= android.app.Notification.DEFAULT_SOUND;
+                        notification.defaults |= android.app.Notification.DEFAULT_LIGHTS;
+                        notification.defaults |= android.app.Notification.DEFAULT_VIBRATE;
 
                         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
                         notificationManager.notify((int) new Date().getTime(), notification);
