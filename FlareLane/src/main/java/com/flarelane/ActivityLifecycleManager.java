11a48ba548aaae46c111678f8a4954d2d71ef090
JP <jpyo0613@gmail.com>
2024-03-26 12:25:07 +0900

    Modify to periodically update the last activation time with a time interval

--- FlareLane/src/main/java/com/flarelane/ActivityLifecycleManager.java
+++ FlareLane/src/main/java/com/flarelane/ActivityLifecycleManager.java
@@ -8,10 +8,29 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class ActivityLifecycleManager {
+    private final Set<Activity> activitySet = new HashSet<>();
+    private final List<Class<?>> skipActivityList = Arrays.asList(
+        PermissionActivity.class,
+        NotificationClickedActivity.class
+    );
+
+    private boolean isSkipActivity(Activity activity) {
+        return skipActivityList.contains(activity.getClass());
+    }
+
     protected Application.ActivityLifecycleCallbacks mActivityLifecycleCallbacks = new Application.ActivityLifecycleCallbacks() {
         @Override
         public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+            if (isSkipActivity(activity)) {
+                return;
+            }
+            activitySet.add(activity);
             FlareLane.deviceRegisterOrActivate(activity.getApplicationContext());
         }
 
@@ -37,6 +56,13 @@ class ActivityLifecycleManager {
 
         @Override
         public void onActivityDestroyed(@NonNull Activity activity) {
+            if (isSkipActivity(activity)) {
+                return;
+            }
+            activitySet.remove(activity);
+            if (activitySet.isEmpty()) {
+                FlareLane.isActivated = false;
+            }
         }
     };
 }
