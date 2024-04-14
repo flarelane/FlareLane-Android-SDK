f37df4c3b75c5cc48123fc920b49e6ef1b832e54
JP <jpyo0613@gmail.com>
2024-04-14 13:14:49 +0900

    -

--- FlareLane/src/main/java/com/flarelane/notification/NotificationClickedButton.kt
+++ FlareLane/src/main/java/com/flarelane/notification/NotificationClickedButton.kt
@@ -1,7 +1,8 @@
 package com.flarelane.notification
 
+import android.os.Bundle
 import android.os.Parcelable
-import com.flarelane.ReflectClass
+import com.flarelane.InteractionClass
 import kotlinx.parcelize.Parcelize
 import org.json.JSONObject
 
@@ -10,10 +11,26 @@ data class NotificationClickedButton(
     @JvmField val id: String,
     @JvmField val label: String,
     @JvmField val link: String
-) : Parcelable, ReflectClass {
+) : Parcelable, InteractionClass {
     constructor(jsonObject: JSONObject) : this(
         jsonObject.getString("buttonId"),
         jsonObject.getString("label"),
         jsonObject.getString("link")
     )
+
+    override fun toHashMap(): HashMap<String, Any?> {
+        return hashMapOf<String, Any?>().also {
+            it["buttonId"] = id
+            it["label"] = label
+            it["link"] = link
+        }
+    }
+
+    override fun toBundle(): Bundle {
+        return Bundle().also {
+            it.putString("buttonId", id)
+            it.putString("label", label)
+            it.putString("link", link)
+        }
+    }
 }
